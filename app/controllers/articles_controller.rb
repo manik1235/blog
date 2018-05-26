5c7e90d497070fd98dba957e5e5df4874f40da05
Andrew Henning-Kolberg <Andrew.Kolberg@gmail.com>
2018-05-26 10:27:09 -0400

    finished thru 5.8. Article listing works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
   end
   
@@ -11,6 +19,7 @@ class ArticlesController < ApplicationController
   
   private
     def article_params
+      # whitelisting the params is required
       params.require(:article).permit(:title, :text)
     end
 end
