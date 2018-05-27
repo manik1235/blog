4ac5493770d678b794346a0b29d4eab44999f8d0
Andrew Henning-Kolberg <Andrew.Kolberg@gmail.com>
2018-05-27 16:14:24 -0400

    finished section 9, security. My blog is 100% hacker proof now.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret1", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
